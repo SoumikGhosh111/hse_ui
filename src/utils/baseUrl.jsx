fe4aee102fca4db440a580d4db06154198aac6ca
SoumikGhosh111 <soumikbuie2001@gmail.com>
2025-03-10 11:04:25 +0530

    temporary change in the sse for frontend testing

--- src/utils/baseUrl.jsx
+++ src/utils/baseUrl.jsx
@@ -1,3 +1,3 @@
-// export const baseUrl = "http://localhost:8000"; 
-export const baseUrl = "https://hse-api.onrender.com";
+export const baseUrl = "http://localhost:8000"; 
+// export const baseUrl = "https://hse-api.onrender.com";
 // export const baseUrl = "https://inspection-app-backend.onrender.com";
